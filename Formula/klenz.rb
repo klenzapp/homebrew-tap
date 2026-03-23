1012daf7f3cb8587fc6f7c808e5d482c24ddd69f
Atul <atul.thapliyal@datadoghq.com>
2026-03-23 15:31:14 -0400

    Update klenz.rb

--- Formula/klenz.rb
+++ Formula/klenz.rb
@@ -6,4 +6,14 @@ class Klenz < Formula
   def install
     ohai "Klenz CLI coming soon"
   end
+
+  def caveats
+    <<~EOS
+      Get started:
+        klenz login    # setup your account, no password needed
+        klenz log "grilled chicken with rice"
+
+      No account? Get the app at https://klenz.app
+    EOS
+  end
 end
